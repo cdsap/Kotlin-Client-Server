dc8d90f9cb712d3272844c827b83314daa049bb2
inaki <inaki.villar@agoda.com>
2018-12-04 11:17:28 +0700

    permissions onthe app

--- consumer-server/server/src/main/java/com/kotlin/server/service/Bootstrap.kt
+++ consumer-server/server/src/main/java/com/kotlin/server/service/Bootstrap.kt
@@ -1,6 +1,9 @@
 package com.kotlin.server.service
 
+import com.googlecode.objectify.Objectify
 import com.googlecode.objectify.ObjectifyService
+import com.googlecode.objectify.ObjectifyService.ofy
+import com.googlecode.objectify.Ref
 import com.kotlin.server.repository.database.PairStore
 import com.kotlin.server.repository.database.SymbolStore
 import com.kotlin.server.repository.database.TradeStore
@@ -26,9 +29,8 @@ class Bootstrap : ServletContextListener {
     }
 
     override fun contextDestroyed(sce: ServletContextEvent?) {
-  //      TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //      TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
-
 
 
     private fun initData() {
@@ -36,31 +38,14 @@ class Bootstrap : ServletContextListener {
         //                .count() == 0) {
         System.out.println("21www11")
 
-//        ObjectifyService.ofy().save().entity(SymbolStore("THB")).now()
-//        ObjectifyService.ofy().save().entity(SymbolStore("BTC")).now()
-//        ObjectifyService.ofy().save().entity(SymbolStore("OMG")).now()
-//        ObjectifyService.ofy().save().entity(SymbolStore("XRP")).now()
-//        ObjectifyService.ofy().save().entity(SymbolStore("ETH")).now()
 
-//        ObjectifyService.ofy().save().entity(PairStore(
-//                1,
-//                Ref.create(ObjectifyService.ofy().load().type(SymbolStore::class.java).id("THB").safe()),
-//                Ref.create(ObjectifyService.ofy().load().type(SymbolStore::class.java).id("BTC").safe())))
-//
-//        ObjectifyService.ofy().save().entity(PairStore(
-//                25,
-//                Ref.create(ObjectifyService.ofy().load().type(SymbolStore::class.java).id("THB").safe()),
-//                Ref.create(ObjectifyService.ofy().load().type(SymbolStore::class.java).id("XRP").safe())))
-//
-//        ObjectifyService.ofy().save().entity(PairStore(
-//                26,
-//                Ref.create(ObjectifyService.ofy().load().type(SymbolStore::class.java).id("THB").safe()),
-//                Ref.create(ObjectifyService.ofy().load().type(SymbolStore::class.java).id("OMG").safe())))
-//
-//        ObjectifyService.ofy().save().entity(PairStore(
-//                21,
-//                Ref.create(ObjectifyService.ofy().load().type(SymbolStore::class.java).id("THB").safe()),
-//                Ref.create(ObjectifyService.ofy().load().type(SymbolStore::class.java).id("ETH").safe())))
+        ObjectifyService.run {
+
+ofy().save().entity(SymbolStore("THB"))
+
+      //      ofy().factory()
+//                    Ref.create(ObjectifyService.ofy().load().type(SymbolStore::class.java).id("ETH").safe())))
+        }
 
     }
 
